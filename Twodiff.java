19a9288e2928ae3ca4302836fcd3be39d25f2402
neekunjchaturvedi <neekunjchaturvedi3@gmail.com>
2025-05-31 11:10:45 +0530

    Solved twodiff

---
+++ Twodiff.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Twodiff{
+    public static void main(String[] args) {
+        int []a={12,3,14,56,77,13};
+        int num=13;
+        int diff=2;
+        int count=0;
+        for(int i=0;i<a.length;i++){
+            if(Math.abs(num-a[i])<=diff){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
